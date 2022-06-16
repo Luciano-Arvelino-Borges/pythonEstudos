d5d72c7c179043c8026c6ed86fd2e2c696cfafbb
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-06-15 21:52:57 -0300

    Teste

--- main.py
+++ main.py
@@ -17,16 +17,7 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 =======
 
-
-
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(2+3)
 
 
 >>>>>>> Initial commit
