9382e61f31ac1bba9be83bff67a3ab668367f6b3
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-07-14 00:55:02 -0300

    Commit operadores 1

--- Operadores.py
+++ Operadores.py
@@ -5,6 +5,9 @@ diferenca = numero1 - numero2
 produto = numero1 * numero2
 quociente = numero1 / numero2
 quocienteInteiro = numero1 // numero2
+potencia = numero1**numero2
+raiz = numero1**(1/numero2)
+
 modulo = numero1 % numero2
 print(f'A soma entre {numero1} e {numero2} é {soma}.')
 print(f'A diferença entre {numero1} e {numero2} é {diferenca}.')
@@ -12,4 +15,8 @@ print(f'O produto entre {numero1} e {numero2} é {produto}.')
 print(f'O quociente entre {numero1} e {numero2} é {quociente}.')
 print(f'O quociente inteiro entre {numero1} e {numero2} é {quocienteInteiro}.')
 print(f'O resto da divisão entre {numero1} e {numero2} é {modulo}.')
-print(type(modulo))
+print(f'O potência da base {numero1} e expoente {numero2} é {potencia}.')
+print(f'A raiz enésima real de {numero1} é {raiz}.')
+
+
+
