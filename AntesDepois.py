23b954a4a70322d66f513faf1f01e51faded5275
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-11-27 00:26:11 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Operadores.py

--- AntesDepois.py
+++ AntesDepois.py
@@ -1,2 +1,5 @@
-
+numero = int(input('Digite um número: '))
+ant = numero - 1
+suc = numero + 1
+print(f'O antecessor de {numero} é {ant} e o sucessor é {suc}')
 
---
+++ CalculoDobroTriploeRaiz.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+
+print(f'O dobro do número {numero} é {dobro}, o triplo é {triplo} e a raiz quadrada é {raiz}.')
