ddcf25764d686b93a12e4be2c279a95270aab798
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-11-26 23:40:51 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Operadores.py

--- VerificarDados.py
+++ VerificarDados.py
@@ -1,6 +1,10 @@
 dado = (input('Digite algo: '))
-print(f'O dado apresentado é letra?', (dado.isalpha()))
-print(f'O dado apresentado é número?', (dado.isnumeric()))
+print(f'O dado apresentado é alfabético?', (dado.isalpha()))
+print(f'O dado apresentado é numérico?', (dado.isnumeric()))
 print(f'O dado apresentado é alfanumérico?', (dado.isalnum()))
-print(type(dado))
+print(f'O dado apresentado só tem espaços?', (dado.isspace()))
+print(f'O dado apresentado está em minúsculas?', (dado.islower()))
+print(f'O dado apresentado possui letra(s) maiúscula(s)?', (dado.isupper()))
+print(f'O dado apresentado está em capitalizado?', (dado.istitle()))
+print(f'O tipo primitivo desse dado é?', type(dado))
 
