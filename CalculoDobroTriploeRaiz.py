993ea9a5abe29737760e3e92ade2cdc916008f7b
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2023-05-21 14:02:20 -0300

    teste

--- CalculoDobroTriploeRaiz.py
+++ CalculoDobroTriploeRaiz.py
@@ -5,5 +5,7 @@ raiz = numero ** (1/2)
 
 print(f'O dobro do número {numero} é {dobro}, o triplo é {triplo} e a raiz quadrada é {raiz}.')
 
-
 # ao colocar ":.f", é possível formatar quantas casas decimais serão mostradas no float
+
+
+
